b70f957641936ba81fb5c7a0ebb39e2f6645f5cb
ulrichbusch-work <ulrichbusch2023@gmail.com>
2024-11-09 23:42:27 +0000

    Checked create_message() and analyze_message()  and Draw graph with incoming current values. While drawing graph, the can bus maybe stop

--- motor_protection.py
+++ motor_protection.py
@@ -9,63 +9,56 @@ import time
 import datetime
 
 class CANBusApp:
-    def __init__(self, master):
+    def __init__(self, master):        
         self.master = master
         master.title("CAN Bus Communication")
-        master.geometry("750x900")    
-
-        self.bus = can.interface.Bus(channel='can0', interface='socketcan')  
-
-        self.label = tk.Label(master, text="Enter CAN ID:", anchor='e')
-        self.label.grid(row=0, column=0)
-
+        master.geometry("850x900")         
+        self.check_flag = tk.IntVar()
+        self.check_btn = tk.Checkbutton(master, text="Teaching Mode", variable=self.check_flag, command=self.on_checkbutton_change)    
+        self.check_btn.grid(row=0, column=3, columnspan=1)
+        self.label = tk.Label(master, text="CANID")
+        self.label.grid(row=2, column=0)
         self.can_id_entry = tk.Entry(master)
-        self.can_id_entry.grid(row=0, column=1)
-
-        self.parameter_label = tk.Label(master, text="Parameters:", anchor='e')       
-        self.parameter_label.grid(row=1, column=0)
-
-        self.parameter_entry = tk.Entry(master)
-        self.parameter_entry.grid(row=1, column=1)
-
-        self.alarm_threshold_label = tk.Label(master, text="Alarm Threshold:", anchor='e')
-        self.alarm_threshold_label.grid(row=2, column=0)
-
+        self.can_id_entry.grid(row=3, column=0)
+        self.parameter_label = tk.Label(master, text="Param")       
+        self.parameter_label.grid(row=2, column=2)
+        self.alarm_threshold_label = tk.Label(master, text="Alarm")
+        self.alarm_threshold_label.grid(row=2, column=4)
         self.alarm_threshold_entry = tk.Entry(master)
-        self.alarm_threshold_entry.grid(row=2, column=1)
-
-        self.teaching_button = tk.Button(master, text="Teaching Mode",width=12, height=1, command=self.draw_graph)
-        self.teaching_button.grid(row=0, column=2)
-
-        self.parameter_button = tk.Button(master, text="Parameter", width=12, height=1, command=self.send_message)
-        self.parameter_button.grid(row=1, column=2)
-
-        self.alarm_button = tk.Button(master, text="Alarm Threshold", width=12, height=1,command=self.start_can_listener)
-        self.alarm_button.grid(row=2, column=2)
-
-        self.received_messages = tk.Text(master, height=10, width=50)
-        self.received_messages.grid(row=3, column=1)        
-
-        #self.fig, self.ax = plt.subplots()
-        #self.canvas = FigureCanvasTkAgg(self.fig, self.master)
-        #self.canvas.get_tk_widget().grid(row=4, column=1)
-        self.canvas_width = 600
+        self.alarm_threshold_entry.grid(row=3, column=4)
+        self.status_label = tk.Label(master, text="Status")
+        self.status_label.grid(row=2, column=6)
+        self.status_entry = tk.Entry(master)
+        self.status_entry.grid(row=3, column=6)           
+        self.id_button = tk.Button(master, text="Start", width=6, height=1, command=self.transceive_message)
+        self.id_button.grid(row=3, column=1)
+        self.received_messages = tk.Text(master, height=5, width=15)
+        self.received_messages.grid(row=3, column=2, rowspan=2)  
+        self.parameter_button = tk.Button(master, text="Update", width=6, height=1, command=self.transceive_message)
+        self.parameter_button.grid(row=3, column=3)
+        self.alarm_button = tk.Button(master, text="Update", width=6, height=1,command=self.transceive_message)
+        self.alarm_button.grid(row=3, column=5)        
+        self.canvas_width = 800
         self.canvas_height = 400
         self.canvas = tk.Canvas(master, width=self.canvas_width, height=self.canvas_height, bg="white")
-        self.canvas.grid(row=4, columnspan=3)
-
-        self.start_button = tk.Button(self.master, text="Start Simulation", command=self.start_simulation)
-        self.start_button.grid(row=5, column=1)
-
-        self.start_can_listener()
+        self.canvas.grid(row=6, columnspan=7)
+        self.start_button = tk.Button(self.master, text="Start", command=self.start_simulation)
+        self.start_button.grid(row=7, column=1)
+        self.bus = can.interface.Bus(channel='can0', interface='socketcan') 
+        self.start_threads()
         self.can_values = [0 for _ in range(100)]
         self.max_values = 100
-        self.simulation_running = False
-        
+        self.simulation_running = False        
         self.event = threading.Event()
-        self.slave_ID = ""
-        
+        self.slave_ID = ""        
         self.can_message = CANMSG()
+        self.start_simulation()
+    def on_checkbutton_change(self):
+        #This function is invoked when the checkbox is checked/unchecked
+        if self.check_flag.get():
+            print("Checkbox is checked")
+        else:
+            print("Checkbox is unchecked")  
 
     def start_simulation(self):
         self.simulation_running = True
@@ -74,8 +67,8 @@ class CANBusApp:
     def update_graph(self):
         if self.simulation_running:
             # Simulate receiving a new CAN bus value (0-100)
-            new_value = random.randint(0, 100)
-            self.can_values.append(new_value)
+            #new_value = random.randint(0, 100)
+            #self.can_values.append(new_value)
 
             # Keep the list size within max_values
             if len(self.can_values) > self.max_values:
@@ -124,59 +117,46 @@ class CANBusApp:
 
                 # Draw the line connecting the two points
                 self.canvas.create_line(x1, y1, x2, y2, fill="blue", width=2)
+    def start_threads(self):
+        self.sender_thread = threading.Thread(target=self.transceive_message)
+        self.sender_thread.daemon = True
+        self.sender_thread.start() 
 
-    def send_message(self):
-        message_send_flag = True
+    def transceive_message(self):
         while True:
             if self.can_id_entry.get() != "":
                 can_id = int(self.can_id_entry.get(), 16)  # Getting CAN ID in hex format
                 print(f"Entry checking")
-            self.can_message.create_message(self.can_message.msg_type[0])   
-            message = self.can_message.can_msg_data;  
-            print("message", message)
-            if message:
-                msg = can.Message(arbitration_id=0x01, data=message.encode())
-                time.sleep(0.0005)
-                #print(message)
+            self.can_message.create_message(self.can_message.msg_type_dict["status"])   
+            smsg = self.can_message.can_msg_data
+            if smsg:
+                msg = can.Message(arbitration_id=0x01, data=smsg)
+                #time.sleep(0.01)
                 try:
-                    #print(f"Message {msg} sent with CAN ID {11}")
-                    self.bus.send(msg)              
-                    message_send_flag = True
+                    print(f"Message {msg} sent with CAN ID {11}")
+                    self.bus.send(msg)
                 except can.CanError:
-                    message_send_flag = False
                     print("Message NOT sent", can.CanError)
                     print("msg", msg)
-            self.event.wait()
-            print("event emitted")
+            #self.event.wait()
+            rmsg = self.bus.recv(1)
+            if rmsg is None:
+                print("No message received")
+                continue
+                #rmessage = self.bus.recv() 
+            rmsg_id = rmsg.arbitration_id - 0xf00
+            if rmsg_id == 0x01:                    
+                received_message = f"Received message: ID: {hex(rmsg.arbitration_id)}, Data: {rmsg.data}\n"
+                print("received message", received_message)
+                self.can_values.append(self.can_message.analyze_message(rmsg.data))
     
     def open_parameter_popup(self):
         self.popup = ParameterSetting(self.master, self)
 
-
-    def start_can_listener(self):
-        self.sender_thread = threading.Thread(target=self.send_message)
-        self.sender_thread.daemon = True
-        self.sender_thread.start()        
-        self.listener_thread = threading.Thread(target=self.receive_messages)        
-        self.listener_thread.daemon = True
-        self.listener_thread.start()
-
-    def receive_messages(self):
-        while True:
-            message = self.bus.recv()
-            if message:
-                if message.arbitration_id - 0xf00 == 0x01:
-                    self.event.set()
-                received_message = f"Received message: ID: {hex(message.arbitration_id)}, Data: {message.data}\n"
-                self.parameter_entry.delete(0,tk.END)
-                self.parameter_entry.insert(0, message.arbitration_id)
-                #self.parameter_entry.see(tk.END)
-                self.received_messages.insert(tk.END, received_message)
-                self.received_messages.see(tk.END)  # Scroll to the end
 class CANMSG:
     def __init__(self):
         # Can Flag byte
-        self.msg_type = { 
+        self.msg_type_dict = { 
             "status": 0x01, 
             "alarm_threshold": 0x02, 
             "rw_setting": 0x04,
@@ -207,14 +187,14 @@ class CANMSG:
     def create_message( self, msg_type ):
         self.can_msg_data.clear()
         self.can_msg_data.append(msg_type)
-        if msg_type == self.dict_flag_byte["status"]:
+        if msg_type == self.msg_type_dict["status"]:
             print("status")
         
-        elif msg_type == self.dict_flag_byte["alarm_threshold"]:
+        elif msg_type == self.msg_type_dict["alarm_threshold"]:
             print("alarm_threshold")
             self.can_msg_data.append(self.alarm_threshold_current / 100)
             self.can_msg_data.append(self.alarm_threshold_current % 100)
-        elif msg_type == self.dict_flag_byte["rw_setting"]:
+        elif msg_type == self.msg_type_dict["rw_setting"]:
             print("rw_setting")
             self.can_msg_data.append(self.rw_dict["P"])
             self.can_msg_data.append(self.rw_dict["I"])
@@ -222,7 +202,7 @@ class CANMSG:
             self.can_msg_data.append(self.rw_dict["T"])
             self.can_msg_data.append(self.rw_dict["Calibration"])
             self.can_msg_data.append(self.rw_dict["RW"])
-        elif msg_type == self.dict_flag_byte["RTC"]:
+        elif msg_type == self.msg_type_dict["RTC"]:
             print("RTC")
             current_time = datetime.datetime.now()
             self.can_msg_data.append(current_time.hour)
@@ -231,7 +211,7 @@ class CANMSG:
             self.can_msg_data.append(current_time.day)
             self.can_msg_data.append(current_time.month)
             self.can_msg_data.append(current_time.year % 100)
-        elif msg_type == self.dict_flag_byte["history"]:
+        elif msg_type == self.msg_type_dict["history"]:
             print("History")
             self.can_msg_data.append(self.dict_history["max_current"] / 100)
             self.can_msg_data.append(self.dict_history["max_current"] % 100)
@@ -240,17 +220,20 @@ class CANMSG:
             self.can_msg_data.append(self.dict_history["average"] / 100)
             self.can_msg_data.append(self.dict_history["average"] % 100)
         
-    def analyze_message(self, received_ID, received_msg):
+    def analyze_message(self, received_msg):
         print("analyze_message")
-        if self.received_msg[0] == self.dict_flag_byte["status"]:
-            print("status")
-        elif self.received_msg[0] == self.dict_flag_byte["alarm_threshold"]:
+        if received_msg[0] == self.msg_type_dict["status"]:
+            print("status message from Adafruit")
+            current = received_msg[1] + received_msg[2] / 100
+            print("current", current)
+            return current
+        elif received_msg[0] == self.msg_type_dict["alarm_threshold"]:
             print("alarm_threshold")
-        elif self.received_msg[0] == self.dict_flag_byte["rw_setting"]:
+        elif received_msg[0] == self.msg_type_dict["rw_setting"]:
             print("rw_setting")
-        elif self.received_msg[0] == self.dict_flag_byte["RTC"]:
+        elif received_msg[0] == self.msg_type_dict["RTC"]:
             print("RTC")
-        elif self.received_msg[0] == self.dict_flag_byte["history"]:
+        elif received_msg[0] == self.msg_type_dict["history"]:
             print("history")
 
 
